bd5708c5a2d0255836279862b9303061a4bc69ec
tejas <pytuner@outlook.com>
2024-08-23 21:19:04 +0530

    api integration, table bind

---
+++ src/components/EmpCRUD/EmpAddComponent/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmpAddComponent = () => {
+  return (
+    <div>EmpAddComponent</div>
+  )
+}
+
+export default EmpAddComponent;
--- src/pages/EmpDirectory/index.js
+++ src/pages/EmpDirectory/index.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Swal from 'sweetalert2';
+import EmpTableComponent from '../../components/EmpCRUD/EmpTableComponent';
+import EmpAddComponent from '../../components/EmpCRUD/EmpAddComponent';
+import EmpEditComponent from '../../components/EmpCRUD/EmpEditComponent';
+
+
+
+const get_employeeData = async (set_EmpList) => {
+  try {
+    const result = await axios.get('	http://localhost:8000/employeeRegistory/get')
+    // console.log(result.data);
+    set_EmpList(result.data);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+};
+
 
 const EmpDirectory = () => {
+
+  // state variables
+  const [employeeList, set_EmployeeList] = useState(null);
+  const [selectedEmployee, set_SelectedEmployee] = useState(null);
+  const [isAddEmployee, set_IsAddEmployee] = useState(false);
+  const [isEditEmployee, set_IsEditEmployee] = useState(false);
+
+
+  // effects
+  useEffect(() => {
+    get_employeeData(set_EmployeeList);
+  }, []);
+
+  // handle functions
+  const handle_EditEmployee = () => {
+    // handle edit employee fn
+  }
+
+  const handle_DeleteEmployee = () => {
+    // handle delete employee fn
+  }
+
   return (
     <div>
-      emp_directory
+      {/* diplay table */}
+      {!isAddEmployee && !isEditEmployee &&
+        (
+          <EmpTableComponent
+            empList={employeeList}
+            handleEditEmp={handle_EditEmployee}
+            handleDeleteEmp={handle_DeleteEmployee}
+          />
+        )
+      }
+      {isAddEmployee &&
+        (
+          <EmpAddComponent
+            empList={employeeList}
+            setEmpList={set_EmployeeList}
+            setIsAdding={set_IsAddEmployee}
+          />
+        )
+      }
+      {isEditEmployee &&
+        (
+          <EmpEditComponent
+            empList={employeeList}
+            setEmpList={set_EmployeeList}
+            setIsEditing={set_IsEditEmployee}
+          />
+        )
+      }
     </div>
   )
 }
 
-export default EmpDirectory
+export default EmpDirectory;
